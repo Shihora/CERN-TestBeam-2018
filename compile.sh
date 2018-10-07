903ef48e0b035873cb625eabc4226df42997e378
Shihora <linusshihora@gmail.com>
2018-10-07 21:36:39 +0200

    remove example comment

--- compile.sh
+++ compile.sh
@@ -2,6 +2,5 @@
 
 rm read
 g++ geometry.C read.C analysis.C main.C `root-config --libs --cflags` -o read
-#new comment
 #compile for memory test
 #g++ read.C `root-config --libs --cflags` -o read  -ltcmalloc
