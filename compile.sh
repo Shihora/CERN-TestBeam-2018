ee8ede65b5a298f2cb489f2534da796eeebe9969
schliwiju <schliwij@hu-berlin.de>
2018-10-07 21:44:12 +0200

    my first commit

--- compile.sh
+++ compile.sh
@@ -2,6 +2,7 @@
 
 rm read
 g++ geometry.C read.C analysis.C main.C `root-config --libs --cflags` -o read
+# foo comment
 #new comment
 #compile for memory test
 #g++ read.C `root-config --libs --cflags` -o read  -ltcmalloc
