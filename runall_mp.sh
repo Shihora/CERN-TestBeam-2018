fff1adbb2545f40fa2c15b14d6e8148c89aecd98
schliwiju <julian@schliwi.de>
2018-10-23 15:00:24 +0200

    added cut command to extract runNr form file name

--- runall_mp.sh
+++ runall_mp.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 #usage: ./runall_mp.sh
 
+# No runlist needed. Fetches list of files to be processed by "ls" command
 
 work_data()
 {
 here=`pwd`
 runName=$0
-# echo $runName # dummy functionality for debugging
 
-mkdir $here/runs/$runName
+echo $runName # dummy functionality for debugging
 
-if [ ! -e $here/runs/$runName/$runName.list ]; then
-	ls $here/data/$runName | grep \.bin > $here/runs/$runName/$runName.list
-fi
+# mkdir $here/runs/$runName
 
-time $here/read $here/runs/$runName/$runName.list $here/data/$runName/ $here/runs/$runName/out.root run_counter
+# if [ ! -e $here/runs/$runName/$runName.list ]; then
+# 	ls $here/data/$runName | grep \.bin > $here/runs/$runName/$runName.list
+# fi
+
+# inFileList=$here/runs/$runName/$runName.list
+inDataFolder=$here/data/$runName/
+# outFile=$here/runs/$runName/out.root
+runNr=$(echo $runName | cut -d "_" -f 1)
+echo $runNr
+
+# time $here/read $here/runs/$runName/$runName.list $here/data/$runName/ $here/runs/$runName/out.root run_counter
 }
 
 # temporary declare bash function "work_data" to PATH variable
@@ -25,3 +33,4 @@ export -f work_data
 # "-P 8" option specifies number of parallel threads used
 # xargs utilizes bash that executes function "work_data" 
 ls ./data | xargs -n 1 -P 8 bash -c 'work_data'
+
