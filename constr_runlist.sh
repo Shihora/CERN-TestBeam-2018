43601552475ba3f3918d71501ed5f441022c167a
schliwiju <julian@schliwi.de>
2018-10-25 18:59:57 +0200

    Alpha version: parse names of WC directories in ./data and construct runlist

---
+++ constr_runlist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#usage: ./constr_runlist.sh
+
+# currently implemented scheme: runNr runName pos pdgID energy angle
+
+# scheme 1: runNr_particleName_posNr
+# scheme 2: runNr_particleNameEnergyvalue_posNr
+# scheme 3: runNr_particleNameEnergyvalue_posNr_WOMpos
+# scheme 4: runNr_particleNameEnergyvalue_posNr_angleValue_WOMpos
+# scheme 5: runNr_particleNameEnergyvalue_scanBD_horizPos_vertPos_WOMpos
