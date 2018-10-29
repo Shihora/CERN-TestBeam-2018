d05293f2356c74b763df3de49190b2d3f6cd44c7
schliwiju <julian@schliwi.de>
2018-10-29 13:28:03 +0100

    added hardcoded MP for runs 88-107. bug fixes

--- constr_runlist.sh
+++ constr_runlist.sh
@@ -3,11 +3,13 @@
 
 # 1st argument: Path to runlistfile
 
-# currently implemented scheme in main.C: runNr runName pos pdgID energy angle (supports runs 29-75)
-# this script creats a runlist supporting runs 19-107
+# Currently implemented scheme in main.C: runNr runName pos pdgID energy angle (supports runs 29-75)
 
+# This script creates a runlist supporting runs 19-107 outputting two schemes:
+# runs 19-87  -> runNr runName MP pdgID energy angle WCidentifier
+# runs 88-107 -> runNr runName MP pdgID energy angle WCidentifier sidePostitionX sidePostitionY
 
-# LIST of naming schemes:
+# LIST of existing naming schemes:
 # runNr 1-18 ->
 # "1_18_dist_scan_long"
 # runNr 19-28 ->
@@ -37,6 +39,7 @@ parse ()
 	dir_name=$0
 	rl_file=$1
 
+	# construct array. contains the elements separated by "_" delimiter in $dir_name
 	IFS="_" read -r -a fields <<< "$dir_name"
 	
 	nfields=${#fields[@]}
@@ -67,12 +70,34 @@ parse ()
 	# get measurement position
 	runMP=${fields[2]}
 	case $runMP in
-		pos[0-9] | pos[1][0-9] )
-			front_pos=$(echo $runMP | cut -c 4-)
+		pos[0-9] | pos[1][0-9] ) # 0°/30° measurements, no xy-coordinates
+			frontMP=$(echo $runMP | cut -c 4-)
 			;;
-		scanBD)
+		scanBD) # 90° WOM scans, hardcoded $sideMP
 			side_pos_x=${fields[3]}
 			side_pos_y=${fields[4]}
+			case $side_pos_y in
+				0)
+					case $side_pos_x in
+						0)sideMP=18;; 6)sideMP=19;; 12)sideMP=20;; 18)sideMP=21;; 24)sideMP=22;;
+					esac
+					;;
+				1)
+					case $side_pos_x in
+						0)sideMP=23;; 6)sideMP=24;; 12)sideMP=25;; 18)sideMP=26;; 24)sideMP=27;;
+					esac
+					;;
+				2)
+					case $side_pos_x in
+						0)sideMP=28;; 6)sideMP=29;; 12)sideMP=30;; 18)sideMP=31;; 24)sideMP=32;;
+					esac
+					;;
+				3)
+					case $side_pos_x in
+						0)sideMP=33;; 6)sideMP=34;; 12)sideMP=35;; 18)sideMP=36;; 24)sideMP=37;;
+					esac
+					;;
+			esac
 			;;
 		*)
 			echo "UNKNOWN position description in run $runNr"
@@ -101,10 +126,10 @@ parse ()
 	# get WaveCatcher identifier
 	case $nfields in
 		3)
-			# continue
+			WC="AB"
 			;;
 		[4-7])
-			WC=${fields[@]: -1:1} # accesses last element in fields.
+			WC=${fields[@]: -1:1} # accesses last element in ${fields}.
 			;;
 		*)
 			echo "UNKNOWN WaveCatcher identifier in run $runNr"
@@ -116,14 +141,12 @@ parse ()
 	######################
 	# line_to_runlsit
 	case $nfields in
-		3)
-			line_to_runlsit="$runNr $dir_name $front_pos $pdgID $energy";;
-		4)
-			line_to_runlsit="$runNr $dir_name $front_pos $pdgID $energy $WC";;
-		5)
-			line_to_runlsit="$runNr $dir_name $front_pos $pdgID $energy $angle $WC";;
+		[3-5])
+			line_to_runlsit="$runNr $dir_name $frontMP $pdgID $energy $angle $WC"
+			;;
 		7)
-			line_to_runlsit="$runNr $dir_name $side_pos_x $side_pos_y $pdgID $energy $angle $WC";;			
+			line_to_runlsit="$runNr $dir_name $sideMP $pdgID $energy $angle $WC $side_pos_x $side_pos_y"
+			;;			
 	esac
 
 	echo "$line_to_runlsit"
