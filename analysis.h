637966cec3cf3110f77d4447f293f442abc705e8
schliwiju <julian@schliwi.de>
2018-12-18 17:29:36 +0100

    fixed typo

--- analysis.h
+++ analysis.h
@@ -14,7 +14,7 @@ float integral(TH1F* hWave,float t1,float t2,float BL);
 float* getBL(TH1F* hWave, float* BL, float t1, float t2);
 float* BL_fit(TH1F* hWave, float* BL_chi2, float t1, float t2);
 float PE(TH1F* hWave, float calib_factor, float BL, float t1, float t2);
-loat max_inRange(TH1F* hWave,float t1, float t2);
+float max_inRange(TH1F* hWave,float t1, float t2);
 double amp2pe(double y, float calib_factor, float BL_upper, float BL_lower, float BL_Chi2_upper, float BL_Chi2_lower);
 double correction_function(double x);
 void peakfinder(TH1F *hWave, float t1, float t2, int nPeaks, int sigma, double thr, double *Xarray, double *Yarray, TPolyMarker *pfMarker, bool pfON);
